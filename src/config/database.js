d062f7d18c10757578c0377edbad0bb9ebf0da29
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-07-02 12:02:15 -0300

    feat: adds config database file

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost', 
+    username: 'dockerdb',
+    password: 'dockerdb',
+    database: 'db_api',
+    define: {
+        timestamps: true, 
+        underscored: true
+
+    }
+
+}
